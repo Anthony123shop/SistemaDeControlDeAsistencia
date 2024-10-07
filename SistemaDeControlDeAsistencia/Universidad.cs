c3fd03371958366d0210cd498970718cdd493745
Anthony Pacheco <anthony444pacheco@gmail.com>
2024-10-07 08:04:45 -0600

    Se creo la clase Universidad junto con la clase anidada Estudiante.

--- SistemaDeControlDeAsistencia/Universidad.cs
+++ SistemaDeControlDeAsistencia/Universidad.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace SistemaDeControlDeAsistencia
 {
-    internal class Universidad
+    public class Universidad
     {
+        public class Estudiante
+        {
+            public string Nombre { get; set; }
+            public int TotalSesiones { get; set; }
+            public int SesionesAsistidas { get; set; }
+
+            public Estudiante(string nombre, int totalSesiones, int sesionesAsistidas)
+            {
+                Nombre = nombre;
+                TotalSesiones = totalSesiones;
+                SesionesAsistidas = sesionesAsistidas;
+            }
+        }
 
 
 
